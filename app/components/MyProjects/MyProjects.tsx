efb198e7b4031022d9f32bfe504c7bb823a73217
Diego Garcia <diegg066.99@gmail.com>
2026-06-09 16:17:15 -0600

    add coming soon

---
+++ app/components/MyProjects/MyProjects.tsx
@@ -0,0 +1,7 @@
+export default function MyProjects() {
+  return (
+    <div>
+      <p className="coming-label">coming soon</p>
+    </div>
+  );
+}
---
+++ app/components/Sections/Sections.tsx
@@ -0,0 +1,10 @@
+import MyProjects from "../MyProjects/MyProjects";
+
+export default function Sections() {
+
+  return (
+    <div className="w-full h-60 flex justify-center items-center">
+      <MyProjects />
+    </div>
+  );
+}
